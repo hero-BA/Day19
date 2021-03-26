892ba2ef7c39ba06b40efa47baf133307d1c98da
hero-BA <suad.hero@gmail.com>
2021-03-26 18:48:33 +0100

    Added exercise: Create React Boilerplate

---
+++ ex03/src/index.js
@@ -0,0 +1,21 @@
+// src/index.js
+
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+
+// add the following code below this line
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, Padawans!</h1>
+      </div>
+    );
+  }
+}
+
+// add the following code below this line
+
+ReactDom.render(<App />, document.getElementById("root"));
